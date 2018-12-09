1f768bb36dbcf9510fb77ed8afdbbcbc92a15e35
Swapnil Vatsa <lmaoswap@gmail.com>
2018-12-10 01:37:57 +0530

    Version 1

---
+++ app/src/main/java/com/example/swapnil/authemailfirebase_basic/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.example.swapnil.authemailfirebase_basic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    
+    TextView register,forgotEmail,resendMail;
+    EditText email,password;
+    Button button;
+    ImageView profile_imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+        register = findViewById(R.id.registerTextView);
+        forgotEmail = findViewById(R.id.forgotTextView);
+        resendMail = findViewById(R.id.resendEmailTextView);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        
+        
+        resendMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "What The Fuck", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/com/example/swapnil/authemailfirebase_basic/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.swapnil.authemailfirebase_basic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
